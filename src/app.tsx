e37392b92d0140112a307ae0907c3634f86f7b98
CutzuDev <alexfarkas05@gmail.com>
2024-01-07 21:41:19 +0200

    updt

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,9 @@ const root = createRoot(document.body);
 root.render(<Main />);
 
 function Main() {
+  const [consoleList, setConsoleList] = useState([]);
   const [appPath, setappPath] = useState();
+  const [filePath, setFilePath] = useState();
   const [devToolsOpen, setDevToolsOpen] = useState(false);
 
   function enableDev() {
@@ -14,14 +16,25 @@ function Main() {
     ipcRenderer.send("dev", !devToolsOpen);
   }
 
+  // async function getFilePath() {
+  //   const filePath = await ipcRenderer.invoke("get-txt-file");
+  //   setFilePath(filePath);
+  //   setConsoleList([...consoleList, filePath]);
+  // }
+  async function getFilePath() {
+    const filePath = await ipcRenderer.invoke("get-txt-file");
+    // setFilePath(filePath);
+    // setConsoleList([...consoleList, filePath]);
+  }
+
   async function getPath() {
     const appPath = await ipcRenderer.invoke("getAppPath");
-    console.log(appPath);
     setappPath(appPath);
+    setConsoleList([...consoleList, appPath]);
   }
   return (
     <>
-      <nav className="w-full p-2.5 bg-sky-900  gap-5 flex justify-center items-center">
+      <nav className="w-full p-2.5 bg-sky-900 gap-5 flex justify-center items-center">
         <button
           className="px-3 py-1 bg-sky-800 border border-sky-500 rounded-md min-w-20"
           onClick={() => {
@@ -38,8 +51,20 @@ function Main() {
         >
           Get Path
         </button>
+        <button
+          className="px-3 py-1 bg-sky-800 border border-sky-500 rounded-md min-w-20"
+          onClick={() => {
+            getFilePath();
+          }}
+        >
+          Get File Path
+        </button>
       </nav>
-      <div className="flex flex-col p-2.5">{appPath}</div>
+      <div className="flex flex-col overflow-y-scroll p-2.5">
+        {consoleList.map((el, i) => (
+          <span key={i}>{el}</span>
+        ))}
+      </div>
     </>
   );
 }
